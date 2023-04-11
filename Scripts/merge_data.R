5f83dd3780e2e4ef29b93edb39ddaa1561417ae5
bridgetmarie24 <bridgetbittmann@u.boisestate.edu>
2023-04-11 10:04:58 -0600

    combine continuous and dilution gauging measurements

---
+++ Scripts/merge_data.R
@@ -0,0 +1,11 @@
+# Merge Dilution Gauging and Continuous Datasets #
+
+# Author: Bridget Bittmann
+# Date: 04/11/2023
+
+# Purpose: This script merges data from the continous gauging stations in the DCEW and dilution 
+#         gauging streamflow measurements. Data will then be exported to use in regression analysis.
+
+# Import packages ####
+library(Matrix)
+library(tidyverse)
