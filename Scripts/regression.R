399408b0b5c1304423f416ebb01fdc691351557e
bridgetmarie24 <bridgetbittmann@u.boisestate.edu>
2023-04-11 12:36:03 -0600

    check for normality and run with logged data

--- Scripts/regression.R
+++ Scripts/regression.R
@@ -15,32 +15,36 @@ library(ggpubr) #multiplot figures
 # Set your directory ####
 
 cd <- '~/Desktop/Field Methods/DCEW_streamflow/'
+cd_figures <- '~/Desktop/Field Methods/Figures/'
 
 # Import the data ####
 
-q_vals <- read.csv(paste(cd, 'Data/q_vals.csv', sep=''))
+q_vals <- read.csv(paste(cd, 'Data/all_sites.csv', sep=''))
 q_vals$Date <- as.Date(q_vals$Date)
 
 # Check for normality ####
 hist(q_vals$q)
 shapiro.test(q_vals$q)
-# p-value is 0.32, meaning data not signficantly different from normal dist.
+# p-value is less than 0.05, meaning data is significantly different 
+shapiro.test(log(q_vals$q)) # test on log values
+# p_value is 0.32, so not signficnatly differnet
+q_vals$lt <- log(q_vals$q)
 
 # Linear regression ####
-mod <- lm(q ~ uaa, data = q_vals)
+mod <- lm(lt ~ uaa, data = q_vals)
 summary(mod)
 
 # Visualize the regression ####
-reg <- ggplot(data = q_vals, aes(x = uaa, y = q)) +
+reg <- ggplot(data = q_vals, aes(x = uaa, y = lt)) +
   geom_point() +
   geom_smooth(method = 'lm', fill = 'grey', color = 'black') +
   theme_bw() +
-  ylab('Discharge (L/s)') +
+  ylab('log(Discharge)') +
   xlab('Upslope Accumulated Area (km^2)') +
-  geom_text(x=26.15,y=30, label = 'R2 = 0.11')
+  geom_text(x=10,y=4.5, label = 'R2 = 0.73')
 reg
 
-ggsave(paste(cd, 'Figures/regress.png', sep=''),
+ggsave(paste(cd_figures, 'regress.png', sep=''),
        plot = reg,
        width = 4,
        height = 4)
@@ -53,7 +57,7 @@ reg_ind <- ggplot(data = q_vals, aes(x = uaa, y = q, color = as.character(Date))
   ylab('Discharge (L/s)') +
   xlab('Upslope Accumulated Area (km^2)') 
 reg_ind
-ggsave(paste(cd,'Figures/ind_reg.svg', sep=''),
+ggsave(paste(cd_figures,'ind_reg.svg', sep=''),
        plot = reg,
        width = 6.5,
        height = 4)
