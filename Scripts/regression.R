39197fbe70c54a3027ad1cb5a97763bfec471d55
bridgetmarie24 <bridgetbittmann@u.boisestate.edu>
2023-04-14 11:39:09 -0600

    changed R2 and colors of points

--- Scripts/regression.R
+++ Scripts/regression.R
@@ -11,6 +11,7 @@ library(Matrix) # Gets tidyverse to import
 library(tidyverse) # data manipulation
 library(ggplot2)
 library(ggpubr) #multiplot figures
+library(ggtext)
 
 # Set your directory ####
 
@@ -35,13 +36,21 @@ mod <- lm(lt ~ uaa, data = q_vals)
 summary(mod)
 
 # Visualize the regression ####
+q_vals$ID <- factor(q_vals$ID, levels = c('C1E', 'C2E', 'LG', 'HighMain', 'MidMain', 'LowMain'))
+lab = 'atop(R^2 == 0.73)'
 reg <- ggplot(data = q_vals, aes(x = uaa, y = lt)) +
-  geom_point(aes(colour = ID)) +
+  geom_point(aes(colour = ID), size = 3) +
+  scale_color_manual(values = c('C1E' = 'firebrick4',
+                                'C2E' = 'firebrick1',
+                                'LG' = 'salmon1',
+                                'HighMain' = 'turquoise4',
+                                'MidMain' = 'turquoise',
+                                'LowMain' = 'cadetblue2')) +
   geom_smooth(method = 'lm', fill = 'grey', color = 'black') +
   theme_bw() +
   ylab('log(Discharge)') +
   xlab('Upslope Accumulated Area (km^2)') +
-  geom_text(x=10,y=4.5, label = 'R2 = 0.73') +
+  geom_text(x=10,y=4.5, label = lab, size = 7, parse = TRUE) +
   theme(text = element_text(size=18, family = 'Arial')) # change font size and type
 reg
 
@@ -62,3 +71,7 @@ ggsave(paste(cd_figures,'ind_reg.svg', sep=''),
        plot = reg,
        width = 6.5,
        height = 4)
+
+
+
+
